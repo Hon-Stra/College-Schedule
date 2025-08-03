7904bda80543321f6ee98c00abf23a9616ed521c
Hon-Stra <ralphlexineorea3@gmail.com>
2025-08-04 00:37:10 +0800

    Update data.js

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ import { firstSemSchedule } from './schedules/ioa-bsa-1-1stsem-year-1.js';
 import { spring2026Schedule } from './schedules/eng-ee-1-spring-2026.js';
 import { dhmfirstsemschedule } from './schedules/cthm-dhm-1-1stsem-year-1.js'; 
 import { bsbamYear3FirstSemSchedule } from './schedules/bsba-m-d-1stsem-year-3.js';
-import { bsbafirstsemsched } from './schedules/bsba-mm-1-c-1stsem-year-1';
+import { bsbammfirstsemsched } from './schedules/bsba-mm-1-c-1stsem-year-1';
 
 // Add new schedules imports here as you create new files in the schedules/ folder
 // import { yourNewSchedule } from './schedules/your-new-schedule-file.js';
@@ -13,7 +13,9 @@ export const allSchedules = [
     spring2026Schedule,
     dhmfirstsemschedule,
     bsbamYear3FirstSemSchedule,
+    bsbammfirstsemsched,
     // Add new schedule objects here
     // yourNewSchedule,
 ];
+
 
