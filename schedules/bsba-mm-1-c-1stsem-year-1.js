d5734673b6fa4001edde76deeadcaa4b3754fdf2
Hon-Stra <ralphlexineorea3@gmail.com>
2025-08-04 00:35:09 +0800

    Create bsba-mm-1-c-1stsem-year-1.js

---
+++ schedules/bsba-mm-1-c-1stsem-year-1.js
@@ -0,0 +1,27 @@
+export const bsbammfirstsemsched = {
+  id: 'bsba-mm-1-c-1stsem-year-1',
+  name: 'BSBA-MM-1-C 1st Semester',
+  displayTitleLine1: 'BSBA-MM-1-C | 1st Semester',
+  displayTitleLine2: 'A.Y. 2025-2026',
+  shortId: 'IOA-BSBA-MM-1-C',
+  days: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+  courses: [
+    { time: '03:00 PM', endTime: '04:30 PM', day: 'Monday', name: 'PURPOSIVE COMMUNICATION', room: 'VR' },
+    { time: '01:30 PM', endTime: '03:00 PM', day: 'Monday', name: 'SCIENCE, TECHNOLOGY AND SOCIETY', room: 'VR' },
+    { time: '04:30 PM', endTime: '06:00 PM', day: 'Monday', name: 'PRODUCT MANAGEMENT', room: 'ADMIN - Admin Four Flex B / VR' },
+    { time: '06:00 PM', endTime: '07:30 PM', day: 'Monday', name: 'CONSUMER BEHAVIOR', room: 'ADMIN - Admin Four Flex B / VR' },
+
+    { time: '04:30 PM', endTime: '06:00 PM', day: 'Tuesday', name: 'ART APPRECIATION', room: 'VR' },
+    { time: '03:00 PM', endTime: '04:30 PM', day: 'Tuesday', name: 'INTRODUCTION TO BUSINESS PROCESSING AND OUTSOURCING', room: 'B3 - 403 / VR' },
+    { time: '06:00 PM', endTime: '07:30 PM', day: 'Tuesday', name: 'MAKATI HERITAGE STUDIES 1', room: 'VR' },
+
+    { time: '03:00 PM', endTime: '04:30 PM', day: 'Thursday', name: 'PURPOSIVE COMMUNICATION', room: 'VR' },
+    { time: '01:30 PM', endTime: '03:00 PM', day: 'Thursday', name: 'SCIENCE, TECHNOLOGY AND SOCIETY', room: 'VR' },
+
+    { time: '04:30 PM', endTime: '06:00 PM', day: 'Friday', name: 'ART APPRECIATION', room: 'VR' },
+    { time: '01:30 PM', endTime: '03:00 PM', day: 'Friday', name: 'SCIENCE, TECHNOLOGY AND SOCIETY', room: 'VR' },
+    { time: '11:00 AM', endTime: '01:00 PM', day: 'Friday', name: 'PHYSICAL ACTIVITIES TOWARD HEALTH AND FITNESS 1 (PATHFIT 1)', room: 'OVAL - Track Oval A' },
+
+    { time: '10:00 AM', endTime: '01:00 PM', day: 'Saturday', name: 'CWTS 1', room: 'B3 - 201' },
+  ],
+};
