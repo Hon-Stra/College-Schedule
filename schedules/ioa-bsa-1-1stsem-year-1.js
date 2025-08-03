cd13b6ab405b8445d21ef5584563785a8e9dc2cb
Hon-Stra <ralphlexineorea3@gmail.com>
2025-08-04 00:50:57 +0800

    Update ioa-bsa-1-1stsem-year-1.js

--- schedules/ioa-bsa-1-1stsem-year-1.js
+++ schedules/ioa-bsa-1-1stsem-year-1.js
@@ -7,24 +7,27 @@ export const firstSemSchedule = {
     shortId: 'IOA-BSA-1',
     days: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
     courses: [
-        { time: '09:00 AM', endTime: '10:30 AM', day: 'Monday', name: 'ART APPRECIATION', room: 'HPSB 1013' },
-        { time: '10:30 AM', endTime: '12:00 PM', day: 'Monday', name: 'PURPOSIVE COMMUNICATION', room: 'HPSB 1014' },
-        { time: '01:30 PM', endTime: '03:00 PM', day: 'Monday', name: 'SCIENCE, TECHNOLOGY AND SOCIETY', room: 'HPSB 1014' },
+        // Monday Classes
+        { time: '09:00 AM', endTime: '10:30 AM', day: 'Monday', name: 'ART APPRECIATION', room: 'HPSB - 1013 / VR-MON' },
+        { time: '10:30 AM', endTime: '12:00 PM', day: 'Monday', name: 'PURPOSIVE COMMUNICATION', room: 'HPSB - 1014 / VR-MON' },
+        { time: '01:30 PM', endTime: '03:00 PM', day: 'Monday', name: 'SCIENCE, TECHNOLOGY AND SOCIETY', room: 'VR-MON, THU' },
 
-        { time: '03:00 PM', endTime: '04:30 PM', day: 'Tuesday', name: 'OPERATION MANAGEMENT AND TQM', room: 'HPSB 1014' },
-        { time: '01:30 PM', endTime: '03:00 PM', day: 'Tuesday', name: 'ECONOMIC DEVELOPMENT', room: 'HPSB 1014' },
-        { time: '09:00 AM', endTime: '12:00 PM', day: 'Tuesday', name: 'FINANCIAL ACCOUNTING & REPORTING 1', room: 'HPSB 1011' },
+        // Tuesday Classes
+        { time: '09:00 AM', endTime: '12:00 PM', day: 'Tuesday', name: 'FINANCIAL ACCOUNTING & REPORTING 1', room: 'HPSB - 1011' },
 
-        { time: '09:00 AM', endTime: '10:30 AM', day: 'Thursday', name: 'ART APPRECIATION', room: 'HPSB 1013' },
-        { time: '10:30 AM', endTime: '12:00 PM', day: 'Thursday', name: 'PURPOSIVE COMMUNICATION', room: 'HPSB 1014' },
-        { time: '01:30 PM', endTime: '03:00 PM', day: 'Thursday', name: 'SCIENCE, TECHNOLOGY AND SOCIETY', room: 'HPSB 1014' },
-        { time: '06:00 PM', endTime: '09:00 PM', day: 'Thursday', name: 'PROFESSIONAL ELECTIVE 1', room: 'HPSB 1014' },
-        { time: '03:00 PM', endTime: '05:00 PM', day: 'Thursday', name: 'PHYSICAL ACTIVITIES TOWARD HEALTH AND FITNESS 1', room: 'OVAL Track Oval B' },
+        // Thursday Classes
+        { time: '01:30 PM', endTime: '03:00 PM', day: 'Thursday', name: 'SCIENCE, TECHNOLOGY AND SOCIETY', room: 'VR-MON, THU' },
+        { time: '06:00 PM', endTime: '09:00 PM', day: 'Thursday', name: 'PROFESSIONAL ELECTIVE 1', room: 'VR-THU' },
+        { time: '03:00 PM', endTime: '05:00 PM', day: 'Thursday', name: 'PHYSICAL ACTIVITIES TOWARD HEALTH AND FITNESS 1', room: 'OVAL - Track Oval B' },
 
-        { time: '03:00 PM', endTime: '04:30 PM', day: 'Friday', name: 'OPERATION MANAGEMENT AND TQM', room: 'HPSB 1014' },
-        { time: '01:30 PM', endTime: '03:00 PM', day: 'Friday', name: 'ECONOMIC DEVELOPMENT', room: 'HPSB 1014' },
-        { time: '10:30 AM', endTime: '12:00 PM', day: 'Friday', name: 'MAKATI HERITAGE STUDIES 1', room: 'HPSB 1014' },
+        // Friday Classes
+        { time: '03:00 PM', endTime: '04:30 PM', day: 'Friday', name: 'OPERATION MANAGEMENT AND TQM', room: 'HPSB - 1014' },
+        { time: '01:30 PM', endTime: '03:00 PM', day: 'Friday', name: 'ECONOMIC DEVELOPMENT', room: 'HPSB - 1014' },
+        { time: '10:30 AM', endTime: '12:00 PM', day: 'Friday', name: 'MAKATI HERITAGE STUDIES 1', room: 'VR-FRI' },
+        { time: '03:00 PM', endTime: '04:30 PM', day: 'Friday', name: 'OPERATION MANAGEMENT AND TQM', room: 'HPSB - 1014' },
+        { time: '01:30 PM', endTime: '03:00 PM', day: 'Friday', name: 'ECONOMIC DEVELOPMENT', room: 'HPSB - 1014' },
 
-        { time: '01:30 PM', endTime: '04:30 PM', day: 'Saturday', name: 'CWTS 1', room: 'B3 304' }
+        // Saturday Classes
+        { time: '04:30 PM', endTime: '07:30 PM', day: 'Saturday', name: 'CWTS 1', room: 'B3 - 304' }
     ]
 };
